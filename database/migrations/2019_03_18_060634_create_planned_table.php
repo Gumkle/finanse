292d8eb93a7a5ad9ed533135455358a6043f8dd6
Dawid Dziedzic <dziedzicdev@gmail.com>
2019-03-18 07:36:23 +0100

    Migracje przykladowe

--- database/migrations/2019_03_18_060634_create_planned_table.php
+++ database/migrations/2019_03_18_060634_create_planned_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePlannedTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('planned', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-
-            $table->rememberToken();
+            $table->string('title');
+            $table->float('amount');
+            $table->morphs('object');
+            $table->integer('category_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('planned');
     }
 }
